a940f0c8b152ef2650212220b5adf61252cc64f9
Levi Modl <hammerzaine@gmail.com>
2018-09-15 16:53:25 -0500

    Main menu now working to tell you if program is installed or not. Program now welcomes you as your name if you have one setup, if not welcomes you as your user.

--- CI_Install.py
+++ CI_Install.py
@@ -1,13 +1,35 @@
 #! /usr/bin/python
+#coding:utf8
 
+######################################
+##									##
+##		Collective Industries		##
+##		 Tools Installation			##
+##									##
+##		  By: Levi & Andrew			##
+##				©2018				##
+######################################
+
+
+
+#############
+## Imports ##
+#############
 from sys import platform
 from subprocess import Popen, PIPE
 import distutils.spawn
 import os
 import subprocess
 import getpass
+import pwd
+import getpass
 
-user = getpass.getuser()
+###############
+## Variables ##
+###############
+user = pwd.getpwuid(os.getuid())[4]
+uname = getpass.getuser()
+os_name	= os.name
 
 def prog_check(program):
 	p = Popen(['which', program], stdout=PIPE, stderr=PIPE)
@@ -24,15 +46,35 @@ if platform == "linux" or platform == "linux2":
         oper = file.readlines()
         oper = oper[5].split('=')
         oper = oper[1]
-        
-    print ('######################')
-    print ('##                  ##')
-    print ('##   CI INSTALLER   ##')
-    print ('##                  ##')
-    print ('######################')
-    
-    print ('Welcome ' + user + '. What would you like to install?')
-    
-    print ('1. GitHUB      - ' + str(prog_check('github')))
-    print ('2. GCC       - ' + str(prog_check('gcc')))
-    print ('3. Teamspeak - ' + str(prog_check('teamspeak')))
+
+if uname == '':
+	users = user
+else:
+	users = uname
+	
+def main():
+	print ('######################')
+	print ('##                  ##')
+	print ('##   CI INSTALLER   ##')
+	print ('##                  ##')
+	print ('######################')
+ 
+	print("Welcome " + users + " to the Collective Industries Tools Installation.")
+	print ("Please select what program(s) you would like to install.")
+	print("")
+	print("")
+	print ('1. GitHUB    - ' + str(prog_check('git')))
+	print ('2. GCC       - ' + str(prog_check('gcc')))
+	print ('3. Teamspeak - ' + str(prog_check('teamspeak')))
+	
+	choice = input('Choice: ')
+	
+	if choice == '1':
+		git()
+		
+def git():
+	# GitHub Installation
+	subprocess.call('clear')
+	print('This is were the github installation function will go')
+	
+main()

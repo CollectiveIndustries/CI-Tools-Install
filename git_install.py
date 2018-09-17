bf0b3b39d9b3faf946b628277e76052be352f6b3
Andrew Malone <admmorketh@gmail.com>
2018-09-17 01:47:55 -0500

    adds in unsupported message for "win32" systems. Renames init() to GetOS() and removes uneeded methods.

--- git_install.py
+++ git_install.py
@@ -16,25 +16,30 @@ import os
 def Debug(var1, var2, TF):
 	DEBUG = True
 	if DEBUG == True and TF == True:
-		print(var1 +' = ' + var2)	
+		print(var1 +' = ' + var2)
 		
-def init():
-	os.system('clear')
-	if platform == 'linux' or platform == 'linux2':
-		# linux
-		with open('/etc/os-release') as file:
-			global oper
-			oper = file.readlines()
-			oper = oper[5].split('=')
-			oper = oper[1].strip()
-			
+def GetOS():
+    _SystemOS_ = platform.strip()
+    Debug("startup(): _SystemOS_",_SystemOS_,True)
+
+    if (_SystemOS_ == 'linux' or _SystemOS_ == 'linux2'):
+    # linux
+        with open('/etc/os-release') as file:
+            oper = file.readlines()
+            oper = oper[5].split('=')
+            return oper[1].strip() # Grab OS release Name we want to know what flavor of lenny we use.
+    elif(_SystemOS_ == 'win32'):
+        return _SystemOS_
+
 def main():
-	init()
-	Debug('oper', oper, True)
-	if oper == 'debian':
-		os.system('sudo apt-get -y update')
-		os.system('sudo apt-get -y upgrade')
-		os.system('sudo apt-get -y install git')
-		print('Running Debian OS')
-			
+    oper = GetOS()
+    Debug('oper', oper, True)
+    if(oper == 'debian'):
+        os.system('sudo apt-get -y update')
+        os.system('sudo apt-get -y upgrade')
+        os.system('sudo apt-get -y install git')
+        print('Running Debian OS')
+    else:
+        print("Sorry Windows is not supported at this time :( we are working on it we promise.")
+
 main()	

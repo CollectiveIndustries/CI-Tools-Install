ba83f83bd3ce130d454f5b8bb48f784215747f32
Levi Modl <hammerzaine@gmail.com>
2018-09-27 21:52:55 -0500

    fatal: Not a git repository: lib/../.git/modules/lib

--- cifunction.py
+++ cifunction.py
@@ -13,10 +13,28 @@
 ## Imports ##
 #############
 from lib import com
+import re
+###############
+## Variables ##
+###############
+MyOS =com._OS_()
+oper = MyOS._type_
 
 #################
 ## Main Script ##
 #################
+def word_search(file, word, rword):
+    _file1_ = open(file, 'r')
+    _file2_ = open(file, 'w')
+    #count = 0
+    #for line in _file_:
+    #    count = count + 1
+    #    if re.match('(.*)'+word+'(.*)', line):
+            #print (line,)
+    for line in _file1_:
+        _file1_.write(line.replace(word, rword))
+        _file1_.close()
+        
 def git_install():
 	gname = input('What name would you like to use?: ')
 	if(oper == 'debian'):
@@ -55,57 +73,63 @@ def ssh_install():
 		
 def mysql_install():
     if(oper == 'debian'):
-		os.system('sudo apt-get -y update')
-		os.system('sudo apt-get -y upgrade')
-		os.system('sudo apt-get install -y mysql-server')
-	else:
-		print("Sorry Windows is not supported at this time :( we are working on it we promise.")	
+        os.system('sudo apt-get -y update')
+        os.system('sudo apt-get -y upgrade')
+        os.system('sudo apt-get install -y mysql-server')
+    else:
+        print("Sorry Windows is not supported at this time :( we are working on it we promise.")	
 	
 def mdb_install():
     if(oper == 'debian'):
-		os.system('sudo apt-get -y update')
-		os.system('sudo apt-get -y upgrade')
-		os.system('sudo apt-get install -y mariadb-server')
-	else:
-		print("Sorry Windows is not supported at this time :( we are working on it we promise.")
+        os.system('sudo apt-get -y update')
+        os.system('sudo apt-get -y upgrade')
+        os.system('sudo apt-get install -y mariadb-server')
+    else:
+        print("Sorry Windows is not supported at this time :( we are working on it we promise.")
 	
 	
 def apache_install():
     if(oper == 'debian'):
-		os.system('sudo apt-get -y update')
-		os.system('sudo apt-get -y upgrade')
-		os.system('sudo apt-get install -y apache2')
-	else:
-		print("Sorry Windows is not supported at this time :( we are working on it we promise.")
+        os.system('sudo apt-get -y update')
+        os.system('sudo apt-get -y upgrade')
+        os.system('sudo apt-get install -y apache2')
+    else:
+        print("Sorry Windows is not supported at this time :( we are working on it we promise.")
 	
 def d2u_install():
     if(oper == 'debian'):
-		os.system('sudo apt-get -y update')
-		os.system('sudo apt-get -y upgrade')
-		os.system('sudo apt-get install -y dos2unix')
-	else:
-		print("Sorry Windows is not supported at this time :( we are working on it we promise.")	
+        os.system('sudo apt-get -y update')
+        os.system('sudo apt-get -y upgrade')
+        os.system('sudo apt-get install -y dos2unix')
+    else:
+        print("Sorry Windows is not supported at this time :( we are working on it we promise.")	
 	
 def ip_config():
-	if(oper == 'debian'):
-		nic = raw_input('Are you wanting to setup your WiFi or your Eithernet? [W/E]: ')
-	if nic.lower() == 'w':
+    if(oper == 'debian'):
+        nic = input('Are you wanting to setup your WiFi or your Eithernet? [W/E]: ')
+    if nic.lower() == 'w':
 		# Setting up WiFi IP 
-		cname = raw_input('What is the name of your WiFi Adapter? [wlan0]: ')
-		if cname == '':
-			cname = 'wlan0'
-		else:
-			cname = cname			
-	if nic.lower() == 'e':
-		# Setting up Ethernet IP
-		cname = raw_input('What is the name of your Eithernet Adapter? [eth0]: ')
-		if cname == None:
-			cname = 'eth0'
-			with fileinput.FileInput('/etc/network/interfaces', implace=True, backup='.bak') as file:
-				for line in file:
-					print(line.replace(cname, 'replacement text'))
-		else:
-			cname = cname
-	else:
-		print("Sorry Windows is not supported at this time :( we are working on it we promise.")
+        cname = input('What is the name of your WiFi Adapter? [wlan0]: ')
+        _sdip_ = input('Would you like a Static IP or a Dynamic IP? [S/D]: ')
+        if cname == '':
+            cname = 'wlan0'
+            if _sdip_.lower() == 's':
+                word_search('/etc/network/interfaces', cname+' inet dhcp', cname+'inet static')
+            else:
+                word_search('/etc/network/interfaces', cname+' inet static', cname+'inet dhcp')
+        else:
+            print('good')
+            cname = cname			
+        if nic.lower() == 'e':
+            # Setting up Ethernet IP
+            cname = input('What is the name of your Eithernet Adapter? [eth0]: ')
+        if cname == None:
+            cname = 'eth0'
+            with fileinput.FileInput('/etc/network/interfaces', implace=True, backup='.bak') as file:
+                for line in file:
+                    print(line.replace(cname, 'replacement text'))
+        else:
+            cname = cname
+    else:
+        print("Sorry Windows is not supported at this time :( we are working on it we promise.")
 	

f9a01b838745844f6d214ef3391a4ad709c21e60
Andrew Malone - USS-Havok <admmorketh@gmail.com>
2022-02-02 23:36:23 -0500

    updates full python environment

--- CIToolsInstall.py
+++ CIToolsInstall.py
@@ -40,21 +40,7 @@ DELAY = 2
 ###############
 ## Functions ##
 ###############
-# TODO #4 move this block of code to an SH script. 
-# Frankly if Python3 is _NOT INSTALLED_ how are we running this script?????
-# def init(): 
-#     """Check for and initilize dependancies"""
-#     MyOS.ProgExists('python3')
-#     MyOS.ProgExists('pip')
-#     if MyOS.ProgExists('python3') == 'Not Installed':
-#         os.system('sudo apt-get install -y python3')
-    
-#     global users
-#     if user == '':
-#         users = uname
-#     else:
-#         users = user
-#     main_menu()
+
 
 ###############
 ## Main Menu ##

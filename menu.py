f1fe3397613adcf95a51dd2852b54d55f8e0c5af
Levi Modl <hammer_69_99_99@hotmail.com>
2022-02-06 21:31:02 -0600

    Preparing to write Class section

--- menu.py
+++ menu.py
@@ -20,5 +20,39 @@ from lib import config
 
 
 
-ConfigSectionMap('Menu Options')
+from configparser import ConfigParser
+import os
+
+ConfigFile = "config.d/conf"
+
+conf = ConfigParser()
+conf.read(os.path.abspath(ConfigFile))
+conf.sections()
+
+## Config Parse Helper ##
+
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = conf.options(section)
+    for option in options:
+        try:
+            dict1[option] = conf.get(section, option)
+            if dict1[option] == -1:
+                print("Skipping: %s %s" % (section,option))
+        except:
+            print("Exception on %s %s!" % (section,option))
+            dict1[option] = None
+    return dict1
+
+## Config section writter ##
+def ConfigAddSection(section):
+	conf.add_section(section)
+
+def ConfigSetValue(section,key,value):
+	conf.set(section,key,value)
+
+def ConfigWrite():
+	cfgfile = open(ConfigFile, "wb")
+	conf.write(cfgfile)
+	cfgfile.close()
 
